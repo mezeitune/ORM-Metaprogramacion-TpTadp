78ed93ffdf5572a8a92a577463ec368c2125739d
Nico <nicospam24@gmail.com>
2017-09-13 17:46:06 -0300

    Parte 1 terminada. Faltan testear algunos escenarios

---
+++ spec/test_spec.rb
@@ -0,0 +1,66 @@
+require 'rspec'
+require_relative '../src/orm'
+
+describe 'Create persistent object' do
+
+  let(:pepe){
+    class Person
+      has_one String, named: :first_name, default: ""
+      has_one String, named: :last_name, default: ""
+      has_one Numeric, named: :age, default: 0
+      #has_one Boolean, named: :admin, default: nil FALTA VER TEMA DE Boolean (!!!)
+      #attr_accessor :some_other_non_persistible_attribute
+    end
+    Person.new
+  }
+
+  after(:all) do
+    Class.class_eval("DB.clear_all")
+  end
+
+  it 'A Person should understand persistence methods if Person has one persistent attribute' do
+    expect(pepe).to respond_to(:save!,:refresh!,:forget!)
+  end
+
+  it 'If a Person has one persistent attribute it should have a getter and a setter' do
+    expect(pepe).to respond_to(:first_name,:first_name=)
+  end
+
+  it 'If a Person has one persistent attribute with a default value its getter should return it' do
+    expect(pepe.age).to eq(0)
+  end
+
+  it 'If the first_name of a Person is set as "jose", the getter should return the same name' do
+    pepe.first_name= "jose"
+    expect(pepe.first_name).to eq("jose")
+  end
+
+  it 'After saving a Person, its id should not be nil' do
+    pepe.first_name = "raul"
+    pepe.last_name = "porcheto"
+    pepe.save!
+    expect(pepe.id).not_to be_nil
+  end
+
+  it 'The name of a Person should return to its persisted value after refreshing' do
+    pepe.first_name = "jose"
+    pepe.save!
+    pepe.first_name = "pepe"
+    expect(pepe.first_name).to eq("pepe")
+    pepe.refresh!
+    expect(pepe.first_name).to eq("jose")
+  end
+
+  #it 'Trying to refresh an object that was not previously saved should raise an error' do
+  #  expect(pepe.refresh!).to raise_error('This object does not exist in the database')
+  #end #NO SE ESTÁ CATCHEANDO LA EXCEPCIÓN (!!!)
+
+  it 'After forgetting a Person, its id should be nil' do
+    pepe.first_name = "arturo"
+    pepe.last_name = "puig"
+    pepe.save!
+    expect(pepe.id).not_to be_nil
+    pepe.forget!
+    expect(pepe.id).to be_nil #EL OBJETO NO ESTÁ DESAPARECIENDO DEL REGISTRO EN DISCO (!!!)
+  end
+end
--- src/orm.rb
+++ src/orm.rb
@@ -1,49 +1,60 @@
 require 'tadb'
 
 module Persistible
-  @tabla
-  @hashCampos={}
 
-  def tabla=(unaTabla)
-    @tabla = unaTabla
+  def table=(aTable)
+    @table = aTable
+  end
+
+  def persistentAttributes=(hashPersistentAttributes)#@persistentAttributes contiene los valores propios de la instancia
+    @persistentAttributes = hashPersistentAttributes
+  end
+
+  def id
+    @id
   end
 
   def save!
-    1
+    @id=@table.insert(@persistentAttributes)
   end
 
   def refresh!
-    2
-  end
+    if @id.nil?
+      raise 'This object does not exist in the database'
+    end
+    self.persistentAttributes= @table.entries.find{|entry| entry[:id] == @id}
+    end
 
   def forget!
-    3
+    @table.delete(@id)
+    @id = nil
   end
 end
 
 class Class
-  alias_method :new_sin_persistencia, :new
+  include TADB
+  alias_method :new_no_persistence, :new
 
-  def has_one(type,name)
-    attr_accessor name #Va a convenir tener un hash de atributos persistibles, para no mezclarlos con los otros
+  def has_one(type, hash)
+    name = hash.values.slice!(0)
+    defaultValue = hash.values.slice!(1)
+    #@hashCamposDefault contiene las claves (nombres de atributos) y los valores default que van a tener TODAS las instancias
+    @hashCamposDefault = {} if @hashCamposDefault.nil?
+    @hashCamposDefault[name] = defaultValue #seteo valor por default (y me guardo como clave el nombre del atributo)
     @flagPersistencia = true
   end
 
   def new(*args)
-    nuevaInstancia = self.new_sin_persistencia(*args)#.bind(self).call
+    nuevaInstancia = self.new_no_persistence(*args)
     if @flagPersistencia
       nuevaInstancia.extend(Persistible)
-      nuevaInstancia.tabla = DB.table(self)
+      nuevaInstancia.persistentAttributes= @hashCamposDefault
+      @hashCamposDefault.keys.each do |name|
+        nuevaInstancia.define_singleton_method("#{name}=") {|argument| @persistentAttributes["#{name}".to_sym]=argument} #define el setter
+        nuevaInstancia.define_singleton_method(name) {@persistentAttributes["#{name}".to_sym]} #define el getter
+      end
+      nuevaInstancia.table = DB.table(self)
     end
     nuevaInstancia#Para que devuelva la nueva instancia
   end
 end
-
-#class Person
-#  has_one String, named: :first_name
-#  has_one String, named: :last_name
-#  has_one Numeric, named: :age
-#  has_one Boolean, named: :admin
-#
-#  attr_accessor :some_other_non_persistible_attribute
-#end
