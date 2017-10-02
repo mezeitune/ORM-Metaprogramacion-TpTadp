52379dd81f7107350470816f32e0bf7c05868574
martingsalazar <martin.salazar20@gmail.com>
2017-10-02 17:13:49 -0300

    more tests in recovery_and_search

--- spec/recovery_and_search_spec.rb
+++ spec/recovery_and_search_spec.rb
@@ -8,6 +8,10 @@ class Point
     x = self.x + other.x
     y = self.y + other.y
   end
+  def equal(other)
+    x == other.x
+    y == other.y
+  end
 end
 
 describe 'Recovery and Search' do
@@ -39,13 +43,40 @@ describe 'Recovery and Search' do
     expect(Point.all_instances).to eq([[]])
   end
 
-  #it 'A saved object should appear in all_instances' do
-  #  p1.save!
-  #  puts Point.all_instances
-  #  expect(Point.all_instances.include? p1).to be_truthy
-  #end
+  it 'A saved object should appear in all_instances' do
+    p1.save!
+    pointCopy =  (Point.all_instances).first().first
+    expect(pointCopy.equal(p1)).to be_truthy
+  end
 
-  #AGREGAR MÁS TESTS DE all_instances (!!!)
+  it 'If two Points are persisted, Point.all_instances gives 2 instances of Point class' do
+    p1.save!
+    p2.save!
+    expect((Point.all_instances).first().size).to eq(2)
+  end
+
+  it 'If two Points are persisted, their information is correctly persisted' do
+    p1.save!
+    p2.save!
+    p1Copy = (Point.all_instances).first()[0] # Point.find_by_id(p1.id)
+    p2Copy = (Point.all_instances).first()[1] # Point.find_by_id(p2.id)
+    expect(p1Copy.equal(p1) && p2Copy.equal(p2)).to be_truthy
+  end
+
+  it 'If no Dog instance is persisted and all_instances message is send to Dog class, then it throws NoMethodError' do
+    class Dog
+      owner = "Someone"
+    end
+    expect{Dog.all_instances}.to raise_error NoMethodError
+  end
+
+  it 'all_instances returns the correct quantity after a save and a forget' do
+    p1.save!
+    quantity_after_save = Point.all_instances().first().size
+    p1.forget!
+    quantity_after_forget = Point.all_instances().first().size
+    expect(quantity_after_save == 1 && quantity_after_forget == 0).to be_truthy
+  end
 
   it ':x should be a parameterless method' do
     expect(Point.is_parameterless_method? :x).to be_truthy
